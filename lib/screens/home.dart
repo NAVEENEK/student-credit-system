e21bf82eff4874173f402dae6a70348fb0f1f82e
Naveen EK <naveenerukattil@gmail.com>
2026-02-11 18:11:07 +0530

    add login button

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,3 +1,4 @@
+import 'package:credit_point_system/screens/login_page.dart';
 import 'package:credit_point_system/widget/slider_section.dart';
 import 'package:flutter/material.dart';
 import '../widget/category_section.dart';
@@ -8,29 +9,47 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Events")),
+      appBar: AppBar(
+        title: const Text("Events"),
+        actions: [
+          Padding(
+            padding: EdgeInsetsGeometry.only(right: 12),
+            child: TextButton(
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.green[400]
+              ),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const LoginPage()),
+                );
+              },
+              child: const Text('Login'),
+            ),
+          ),
+        ],
+      ),
       body: SafeArea(
-        child:Column(
+        child: Column(
           children: [
             const SliderSection(),
-            
+
             Expanded(
               child: ListView(
-              padding: EdgeInsets.all(22),
-              children: const [
-                CategorySection(title: "Sports"),
-                CategorySection(title: "Tech"),
-                CategorySection(title: "Arts"),
-                CategorySection(title: "Entertainment"),
-                CategorySection(title: "mech"),
-                CategorySection(title: "electrical"),
-              ],
-                    ),
+                padding: EdgeInsets.all(22),
+                children: const [
+                  CategorySection(title: "Sports"),
+                  CategorySection(title: "Tech"),
+                  CategorySection(title: "Arts"),
+                  CategorySection(title: "Entertainment"),
+                  CategorySection(title: "mech"),
+                  CategorySection(title: "electrical"),
+                ],
+              ),
             ),
           ],
-        ), 
+        ),
       ),
-
     );
   }
 }
